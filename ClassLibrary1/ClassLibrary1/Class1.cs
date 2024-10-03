048facd5073fa44c9e38ac1db4133e4aacb1e610
Artem016 <lenovaega2@gmail.com>
2024-10-03 14:39:56 +0500

    skfhLKJG

--- ClassLibrary1/ClassLibrary1/Class1.cs
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -7,5 +7,34 @@
             Random rnd = new Random();
             return rnd.Next();
         }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return (double)a / b;
+        }
+
+        public int Modulus(int a, int b)
+        {
+            return a % b;
+        }
     }
 }
