ca419d0fc98efa17c9e0fd92610b5695c7f3a6eb
Artem016 <lenovaega2@gmail.com>
2024-10-03 14:27:53 +0500

    kfjkjglwjg

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int GetRandomNumber()
+        {
+            Random rnd = new Random();
+            return rnd.Next();
+        }
+    }
+}
---
+++ ClassLibrary1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibrary1;
+using static ClassLibrary1.Class1;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var class1 = new Class1();
+            Console.WriteLine(class1.GetRandomNumber());
+        }
+    }
+}
